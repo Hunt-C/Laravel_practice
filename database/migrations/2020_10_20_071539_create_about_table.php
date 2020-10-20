a01e21c22d468c1a082714d2c8dde61fd0cfd2ed
user68235 <t20201005@gm.ntpu.edu.tw>
2020-10-20 16:05:39 +0800

    migrate create table

---
+++ database/migrations/2020_10_20_071539_create_about_table.php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::create('about', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+
+        Schema::create('about', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('about');
+    }
+}
+
+//
